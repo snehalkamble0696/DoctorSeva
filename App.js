8d2a8d6675c3978a6bfcc342d433d43b4602744a
snehal kamble <snehalkamble@lovenheal.com>
2023-08-23 19:39:36 +0530

    first commite

---
+++ App.js
@@ -0,0 +1,40 @@
+// How to Detect Call States in React Native App
+// https://aboutreact.com/detect-call-states/
+
+//Import React
+import React, {useState} from 'react';
+
+//Import required component
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Linking,
+  FlatList,
+  SafeAreaView,
+  Image,
+} from 'react-native';
+
+const App = () => {
+
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+       
+        
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+ 
+});
